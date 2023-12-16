f465217847eb0ee965c6685e8a4359e5059d9315
sunghee <h33uid@gmail.com>
2023-12-16 11:12:30 +0900

    set default tokyonight colorscheme style into night

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,12 +1,22 @@
 return {
-  "NvChad/nvim-colorizer.lua",
-  lazy = false,
-  config = function()
-    require("colorizer").setup({
-      filetypes = {
-        "*", -- Highlight all files, but customize some others.
-        cmp_docs = { always_update = true },
-      },
-    })
-  end,
+  {
+    "NvChad/nvim-colorizer.lua",
+    lazy = false,
+    config = function()
+      require("colorizer").setup({
+        filetypes = {
+          "*", -- Highlight all files, but customize some others.
+          cmp_docs = { always_update = true },
+        },
+      })
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+    },
+  },
 }
