f66a4868f6cdc424eda713fc8ba3a96e0ce535c8
sunghee <sunghee@bos-semi.com>
2023-12-18 17:03:18 +0900

    enable undofile(autosave edit history) & delete unnecessary plugin lua script

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,4 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.swapfile = false
+vim.opt.undofile = true
--- lua/plugins/neotree.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    keys = {
-      { "l", false },
-    },
-  },
-}
