d4da39050db60fd0f591483d98b3ad37f39ac2b9
sunghee <h33uid@gmail.com>
2024-01-09 18:34:05 +0900

    change nvim-ts-rainbow to nvim-ts-rainbow2
    1. nvim-ts-rainbow causes error with javascript treesitter
    2. additionally, add plugin for color of current line number

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,5 +1,9 @@
 return {
   {
+    "mawkler/modicator.nvim",
+    config = function() require('modicator').setup() end,
+  },
+  {
     "NvChad/nvim-colorizer.lua",
     lazy = true,
     config = function()
@@ -17,17 +21,19 @@ return {
       "projekt0n/github-nvim-theme",
     },
     opts = { colorscheme = "github_dark" },
+    --opts = { colorscheme = "tokyonight-night" },
   },
   {
-    "mrjones2014/nvim-ts-rainbow",
+    "HiPhish/nvim-ts-rainbow2",
     config = function()
-      require("nvim-treesitter.configs").setup({
+      require'nvim-treesitter.configs'.setup {
         rainbow = {
           enable = true,
-          extended_mode = true,
-          max_file_lines = nil,
-        },
-      })
-    end,
+          -- disable = { 'c', 'cpp' },
+          query = 'rainbow-parens',
+          strategy = require'ts-rainbow'.strategy.global,
+        }
+      }
+    end
   },
 }
