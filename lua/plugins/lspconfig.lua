c6fc87a7adb4ed9692cc5029daa70e1f173c2064
sunghee <h33uid@gmail.com>
2024-04-05 15:46:43 +0900

    fix for finding lsp(clangd) root dir

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,17 +1,28 @@
 local version = vim.inspect(vim.version())
 local current_v = { version["major"], version["minor"], version["patch"] }
 local target_v = { 0, 10, 0 }
+local enabled = false
 if vim.version.gt(current_v, target_v) or vim.version.eq(current_v, target_v) then
-  return {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        inlay_hints = {
-          enabled = true,
-        },
-      },
-    },
-  }
-else
-  return {}
+  enabled = true
 end
+local nvim_lsp = require("lspconfig")
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        enabled = enabled,
+      },
+      servers = {
+        clangd = {
+          filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+          root_dir = function(fname)
+            return nvim_lsp.util.root_pattern('.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json',
+                  'compile_flgas.txt', 'configure.ac', '.git')(fname) or
+                nvim_lsp.util.path.dirname(fname)
+          end,
+        }
+      }
+    },
+  },
+}
