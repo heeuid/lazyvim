10a7d86b433447db0fa55e66fb380d5837bd6c7a
sunghee <h33uid@gmail.com>
2024-04-23 11:53:09 +0900

    remove smartcolumn & faster plugin
    - smartcolumn is the main reason making neovim too slow
    - after removing smartcolumn, neovim is fast enough for big text files

--- lua/plugins/smartcolumn.lua
+++ lua/plugins/smartcolumn.lua
@@ -1,4 +1,4 @@
 return {
-  "m4xshen/smartcolumn.nvim", -- colorcolumn will be shown up when text exceeds a certain columns
-  opts = {},
+  -- "m4xshen/smartcolumn.nvim", -- colorcolumn will be shown up when text exceeds a certain columns
+  -- opts = {},
 }
