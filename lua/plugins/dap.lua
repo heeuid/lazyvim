5c05c015f9b48c7435a019f2da00974e3a197793
Sunghee Kim <h33uid@gmail.com>
2024-05-30 21:50:59 +0900

    solve failed to run config for nvim-dap-python
    it needs debugpy package

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,6 +1,16 @@
 return {
-  "theHamsta/nvim-dap-virtual-text",
-  config = function()
-    require("nvim-dap-virtual-text").setup({})
-  end,
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = {
+      "williamboman/mason.nvim",
+      opts = {
+        ensure_installed = {
+          "debugpy", -- for nvim-dap-python
+        }
+      }
+    },
+    config = function()
+      require("nvim-dap-virtual-text").setup({})
+    end,
+  },
 }
