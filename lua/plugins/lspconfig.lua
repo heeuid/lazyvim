b1d28664104977a70f22964fe20b64f1e75ca4f7
Sunghee Kim <h33uid@gmail.com>
2024-06-24 10:00:34 +0900

    fix to access to a nil value (`nvim_lsp variable`)

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -9,6 +9,7 @@ return {
         clangd = {
           filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
           root_dir = function(fname)
+            local nvim_lsp = require("lspconfig")
             return nvim_lsp.util.root_pattern('.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json',
                   'compile_flgas.txt', 'configure.ac', '.git')(fname) or
                 nvim_lsp.util.path.dirname(fname)
