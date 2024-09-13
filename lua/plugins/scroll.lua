b2ca86700f280a98f4753bd7a9629981819c30e8
Sunghee Kim <h33uid@gmail.com>
2024-09-13 12:39:55 +0900

    update keymap for codewindow

--- lua/plugins/scroll.lua
+++ lua/plugins/scroll.lua
@@ -15,7 +15,21 @@ return {
     config = function()
       local codewindow = require('codewindow')
       codewindow.setup()
-      codewindow.apply_default_keybinds()
+
+      local ok, wk = pcall(require, "which-key")
+
+      if not ok then
+        codewindow.apply_default_keybinds()
+      else
+        wk.add({
+          mode = "n",
+          { '<leader>m', group = "minimap scroll" },
+          { '<leader>mo', codewindow.open_minimap, desc = "Open minimap" },
+          { '<leader>mf', codewindow.toggle_focus, desc = "Toggle minimap focus" },
+          { '<leader>mc', codewindow.close_minimap, desc = "Close minimap" },
+          { '<leader>mm', codewindow.toggle_minimap, desc = "Toggle minimap" },
+        })
+      end
     end,
   }
 }
