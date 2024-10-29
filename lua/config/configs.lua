724f475846e3b8d931299cfa1cc0c6a5c3afdc38
Sunghee Kim <h33uid@gmail.com>
2024-10-29 20:47:50 +0900

    chore: add border of auto-complete window

--- lua/config/configs.lua
+++ lua/config/configs.lua
@@ -1,1 +1,13 @@
 -- run after autocmds.lua
+
+require'lspconfig'.lua_ls.manager.config.settings.Lua.hint = {
+  enable = true
+}
+
+local cmp = require'cmp'
+cmp.setup{
+  window={
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  }
+}
