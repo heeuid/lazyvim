5aa5927e76095e89cc7913dd67fe6e7e1880de1f
sunghee <h33uid@gmail.com>
2025-03-03 18:23:58 +0900

    refactor: add compatibility for telescope utils in ssh plugin

--- lua/plugins/ssh.lua
+++ lua/plugins/ssh.lua
@@ -7,67 +7,45 @@ return {
 
     require("telescope").load_extension("remote-sshfs")
 
-    local util = require("lazyvim.util")
-    local telescope_find_files = util.pick("files", {})
-    local telescope_live_grep = util.pick("live_grep", {})
-    -- local util_telescope = require("lazyvim.util.telescope")
-    -- local telescope_find_files = util_telescope("files", {})
-    -- local telescope_live_grep = util_telescope("live_grep", {})
+    local telescope_find_files
+    local telescope_live_grep
+
+    local ok, util_telescope = pcall(require, "lazyvim.util.telescope")
+    if ok then
+      telescope_find_files = util_telescope("files", {})
+      telescope_live_grep = util_telescope("live_grep", {})
+    else
+      local Util = require("lazyvim.util")
+      telescope_find_files = Util.pick("files", {})
+      telescope_live_grep = Util.pick("live_grep", {})
+    end
+
     local connections = require("remote-sshfs.connections")
 
     local api = require("remote-sshfs.api")
-    local ok, wk = pcall(require, "which-key")
+    vim.keymap.set("n", "<leader>rc", api.connect, { desc = "Remote SSHFS connect" })
+    vim.keymap.set("n", "<leader>rd", api.disconnect, { desc = "Remote SSHFS disconnect" })
+    vim.keymap.set("n", "<leader>re", api.edit, { desc = "Remote SSHFS edit" })
+    vim.keymap.set("n", "<leader>r?", function()
+      print(connections.is_connected())
+    end, { desc = "Remote SSHFS check connection" })
 
-    local find_file = function()
+    -- (optional) Override telescope find_files and live_grep to make dynamic based on if connected to host
+    vim.keymap.set("n", "<leader>ff", function()
       if connections.is_connected() then
         api.find_files({})
       else
         telescope_find_files()
         --builtin.find_files()
       end
-    end
-
-    local live_grep = function()
+    end, { desc = "Find Files (root dir)" })
+    vim.keymap.set("n", "<leader>fg", function()
       if connections.is_connected() then
         api.live_grep({})
       else
         telescope_live_grep()
         --builtin.live_grep()
       end
-    end
-
-    local connect = function()
-      local cmd = vim.fn.input("<user>@<hostname>:<path>")
-      if cmd ~= "" then
-        local host = require'remote-sshfs.utils'.parse_host_from_command(cmd)
-        require'remote-sshfs.connections'.connect(host)
-      else
-        api.connect({})
-      end
-    end
-
-    local is_connected = function() print(connections.is_connected()) end
-
-    if not ok then
-      vim.keymap.set("n", "<leader>rc", connect, { desc = "Remote SSHFS connect" })
-      vim.keymap.set("n", "<leader>rd", api.disconnect, { desc = "Remote SSHFS disconnect" })
-      vim.keymap.set("n", "<leader>re", api.edit, { desc = "Remote SSHFS edit" })
-      vim.keymap.set("n", "<leader>r?", is_connected, { desc = "Remote SSHFS check connection" })
-      vim.keymap.set("n", "<leader>ff", find_file, { desc = "Find Files (root dir)" })
-      vim.keymap.set("n", "<leader>fg", live_grep, { desc = "Grep (root dir)" })
-    else
-      wk.add({
-        mode = "n",
-
-        { "<leader>r",  group = "remote ssh" },
-        { "<leader>rc", connect, desc = "Remote SSHFS connect" },
-        { "<leader>rd", api.disconnect, desc = "Remote SSHFS disconnect" },
-        { "<leader>re", api.edit, desc = "SSH config edit" },
-        { "<leader>r?", is_connected, desc = "Remote SSHFS check connection" },
-
-        { "<leader>ff", find_file, desc = "Find Files (root dir)" },
-        { "<leader>fg", live_grep, desc = "Grep (root dir)" }
-      })
-    end
+    end, { desc = "Grep (root dir)" })
   end,
 }
