fbd7fb779097183c9c1a465ea332805e3b4e8663
sunghee <h33uid@gmail.com>
2025-06-09 16:20:09 +0900

    fix: LazyVim is not ready for mason v2

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,7 +4,8 @@ return {
 
   -- add any tools you want to have installed below
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
+    version = "^1.0.0",
     opts = {
       ensure_installed = {
         "shellcheck",
@@ -24,4 +25,5 @@ return {
       },
     },
   },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 }
