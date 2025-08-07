46b7a8538c0ab56d6a322c032ec7ea68bc6a4823
sunghee <h33uid@gmail.com>
2025-08-08 00:23:40 +0900

    feat: enable all lua hint settings

--- lua/config/configs.lua
+++ lua/config/configs.lua
@@ -10,3 +10,23 @@ end
 -- "debug" for debugging
 vim.lsp.set_log_level("off")
 vim.g.clipboard = "osc52"
+
+local lspconfig
+ok, lspconfig = pcall(require, 'lspconfig')
+if ok then
+  lspconfig.lua_ls.setup {
+    settings = {
+      Lua = {
+        hint = {
+          arrayIndex = true,
+          enable = true,
+          paramName = true,
+          paramType = true,
+          semicolon = true,
+          setType = true,
+        }
+      }
+    }
+  }
+end
+
